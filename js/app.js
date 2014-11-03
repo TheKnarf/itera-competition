80b3d9a1d1528804631f215466fa4fae766e92fe
hkwaller <hkwaller@gmail.com>
2014-11-03 11:32:57 +0100

    directives

--- js/app.js
+++ js/app.js
@@ -83,7 +83,24 @@ app.controller('HomeCtrl', function($scope, $http) {
      link: function(scope, element, attrs, modelCtrl) {
        modelCtrl.$parsers.push(function (inputValue) {
            if (inputValue == undefined) return '' 
-           var transformedInput = inputValue.replace(/[^0-9]/g, ''); 
+           var transformedInput = inputValue.replace(/[^0-9,]/g, ''); 
+           if (transformedInput!=inputValue) {
+              modelCtrl.$setViewValue(transformedInput);
+              modelCtrl.$render();
+           }         
+
+           return transformedInput;         
+       });
+     }
+   };
+})
+.directive('numbersDotOnly', function(){
+   return {
+     require: 'ngModel',
+     link: function(scope, element, attrs, modelCtrl) {
+       modelCtrl.$parsers.push(function (inputValue) {
+           if (inputValue == undefined) return '' 
+           var transformedInput = inputValue.replace(/[^0-9,.]/g, ''); 
            if (transformedInput!=inputValue) {
               modelCtrl.$setViewValue(transformedInput);
               modelCtrl.$render();
