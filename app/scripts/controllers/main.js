92a0526292505a409438312fbc7c81c7b30f9ee7
mikaello <mikaello@ulrik>
2014-11-03 11:18:54 +0100

    conversion of date

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,11 +16,62 @@ angular.module('iteraApp')
     ];
     
     // Parameters to send
+    switch($scope.buttonSelect.startDateMonth) {
+            var loanRaisingMonth = 'loanRaisingMonth=';
+            case 'Jan': 
+                loanRaisingMonth += 1; 
+                break;
+            case 'Feb': 
+                loanRaisingMonth += 2;
+                break;
+            case 'Mar': 
+                loanRaisingMonth += 3;
+                break;
+            case 'Apr': 
+                loanRaisingMonth += 4;
+                break;
+            case 'Mai': 
+                loanRaisingMonth += 5;
+                break;
+            case 'Jun': 
+                loanRaisingMonth += 6;
+                break;
+            case 'Jul': 
+                loanRaisingMonth += 7;
+                break;
+            case 'Aug': 
+                loanRaisingMonth += 8;
+                break;
+            case 'Sep': 
+                loanRaisingMonth += 9;
+                break;
+            case 'Okt': 
+                loanRaisingMonth += 10;
+                break;
+            case 'Nov': 
+                loanRaisingMonth += 11;
+                break;
+            case 'Dec': 
+                loanRaisingMonth += 12;
+                break;
+            default: 
+                loanRaisingMonth += 1;
+            
+    }
+    
+    
     var loanRaisingMonth = "loanRaisingMonth=" + $scope.buttonSelect.startDateMonth;
     var loanRaisingYear = "loanRaisingYear=" + $scope.buttonSelect.startDateYear;
     var prinicpalAmount = "principalAmount=" + $scope.buttonSelect.loan;
     var annualNominalInterestRate = "annualNominalInterestRate=" + $scope.buttonSelect.rate;
     var totalNumberOfPayments = "totalNumberOfPayments=" + $scope.buttonSelect.paymentTime;
+    var fullURL = 'http://localhost/?' +
+        loanRaisingMonth + '&' + 
+        loanRaisingYear + '&' + 
+        prinicpalAmount + '&' + 
+        annualNominalInterestRate + '&' + 
+        annualNominalInterestRate + '&' + 
+        totalNumberOfPayments;
      
     // Simple POST request 
     $http.get('https://cfs-ws-itera.cicero.no/cfp/6/ws/rest/calculator/calculateLoan?' + loanRaisingMonth + '&' + loanRaisingYear + '&' + prinicpalAmount + '&' + annualNominalInterestRate + '&' + annualNominalInterestRate + '&' + totalNumberOfPayments).
