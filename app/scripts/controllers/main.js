edd4bfd8226788b7c404a654e70c43a7c57066f5
mikaello <mikaello@ulrik>
2014-11-03 10:44:24 +0100

    added variables in main controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,5 +14,22 @@ angular.module('iteraApp')
       'AngularJS',
       'Karma'
     ];
+    
+    // Parameters to send
+    var loanRaisingMonth = "loanRaisingMonth=" + $scope.buttonSelect.startDateMonth;
+    var loanRaisingYear = "loanRaisingYear=" + $scope.buttonSelect.startDateYear;
+    var prinicpalAmount = "principalAmount=" + $scope.buttonSelect.loan;
+    var annualNominalInterestRate = "annualNominalInterestRate=" + $scope.buttonSelect.rate;
+    var totalNumberOfPayments = "totalNumberOfPayments=" + $scope.buttonSelect.paymentTime;
+     
+    // Simple POST request 
+    $http.get('https://cfs-ws-itera.cicero.no/cfp/6/ws/rest/calculator/calculateLoan?' + loanRaisingMonth + '&' + loanRaisingYear + '&' + prinicpalAmount + '&' + annualNominalInterestRate + '&' + annualNominalInterestRate + '&' + totalNumberOfPayments).
+    success(function(data, status, headers, config) {
+        console.log(data);
+  }).
+  error(function(data, status, headers, config) {
+        consol.log('error');
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
   });
-
+  });
